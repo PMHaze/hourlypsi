7539a38644c132c22970e12dc9265dde69b1d565
Kheng Hui Yeo <yeokh@i2r.a-star.edu.sg>
2017-06-22 10:22:22 +0800

    Base commit of ZAP volunteers map from Ryan.

---
+++ main.js
@@ -0,0 +1,60 @@
+var geocoder,
+    map,
+    infowindow;
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(1.3537459, 103.8240449);
+    var mapOptions = {
+        zoom: 12,
+        center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(map, "click", function(event) {
+        infowindow.close();
+    });
+    
+    Tabletop.init({
+        key: 'YOUR_API_KEY_HERE',
+        callback: function(data, tabletop) {
+            var address,
+                i = 0;
+
+            function mapNextAddress() {
+                address = data[i]['Postal code'] + ' Singapore';
+                console.log(address);
+                codeAddress(address);
+                if (++i < data.length) {
+                    setTimeout(mapNextAddress, 250);
+                }
+            };
+
+            mapNextAddress();
+        },
+        simpleSheet: true
+    })
+}
+
+function codeAddress(address, centering) {
+    geocoder.geocode({
+        'address': address
+    }, function(results, status) {
+        if (status == 'OK') {
+            if (centering !== undefined && centering)
+                map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+            marker.addListener('click', function() {
+                infowindow.close();
+                infowindow.setContent('<b>Address:</b> ' + address);
+                infowindow.open(map, marker);
+            });
+        } else {
+            console.error('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
